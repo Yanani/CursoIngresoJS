730329921ce825484131dd8ddcd24450bcb93c2d
Yanani <patyanani@gmail.com>
2016-12-05 22:18:13 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -16,3 +16,8 @@ function Mostrar()
 
 
 }//FIN DE LA FUNCIÓN
+
+//Al presionar el botón pedir números hasta que el usuario quiera, mostar:
+//1-Suma de los negativos. 2-Suma de los positivos.
+ //3-Cantidad de positivos. 4-Cantidad de negativos. 
+ //5-Cantidad de ceros. 6-Cantidad de números pares. 7-Promedio de positivos. 8-Promedios de negativos. 9-Diferencia entre positivos y 
