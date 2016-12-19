2bdd6013cda5d1e0357f5371f087ca08eebf98e7
Yanani <patyanani@gmail.com>
2016-12-19 14:27:41 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,7 +1,119 @@
 function Mostrar()
 {
 
-	var contador=0;
+var edad;
+var sexo;
+var minimo;
+var varones;
+var acumulador;
+acumulador=0;
+varones=0;
+
+for (contador=0;contador<2;contador++) 
+	
+{
+
+	edad=prompt("Ingrese edad");
+	edad=parseInt(edad);
+
+	sexo=prompt("ingrese 'm' o 'f'");
+
+	while(edad <0 && edad >100)
+	{
+		edad=prompt("re ingrese edad");
+		edad=parseInt(edad);
+
+	}
+
+    while(sexo!="f" && sexo!="m")
+    	{
+    		sexo=prompt("reingrese sexo");
+    	}
+
+     
+    
+        acumulador= edad+acumulador;
+
+   if (contador==0)
+       {
+       	 minimo=edad
+       }
+    else
+	    	{
+
+  			if(edad<minimo)
+	  				{
+	  					minimo=edad;
+	  				}
+
+
+
+	    	}//else  
+
+    if (sexo=="m" && edad<21)
+
+    {
+    	varones++;
+    }
+
+
+
+
+
+
+
+
+
+
+}//fin for
+
+
+alert("promedio es: " +acumulador/contador);
+alert("baja es: " +minimo);
+alert("varones es: " +varones);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*	var contador=0;
 	// declarar variables
 	
 	var minimo;
@@ -38,9 +150,10 @@ function Mostrar()
 
 document.getElementById('maximo').value=maximo;
 document.getElementById('minimo').value=minimo;
+*/
 
 
-}//FIN DE LA FUNCIÓN
 
 //Al presionar el botón pedir números hasta que el usuario quiera,
 // mostrar el número máximo y el número mínimo.
+}
