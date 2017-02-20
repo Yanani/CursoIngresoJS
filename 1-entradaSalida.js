99bf6ab5806883bbd5a672aa53e0c8480f1e9120
Yanani <patyanani@gmail.com>
2017-02-20 16:03:25 -0300

    Add files via upload

---
+++ 1-entradaSalida.js
@@ -0,0 +1,29 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostrar()
+{
+	
+	
+
+
+var lado;
+lado=0;
+lado=document.getElementById('lado').value;
+lado=parseInt(lado);
+var perimetro;
+perimetro=0;
+perimetro=lado*3;
+
+alert("<br> El perimetro es: " +perimetro);
+
+
+
+
+
+}
+
+
+
+/*Mostrar
+Enunciado:
+
+-Realizar el algoritmo que al presionar el botón "Mostrar" lea la base de un triangulo equilatero por ID, e informar el perímetro del mismo por alert .*/
