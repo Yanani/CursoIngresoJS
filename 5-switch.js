4598d40cf3bb9f3eac2556bd3080644d8ba765c7
Yanani <patyanani@gmail.com>
2017-02-20 16:04:08 -0300

    Add files via upload

---
+++ 5-switch.js
@@ -0,0 +1,71 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostrar()
+{
+	
+var mes;
+mes=prompt("Ingrese mes del año");
+
+
+/*if (mes=="enero" || mes=="febrero" )
+{
+	alert("“Veranito!!");
+}
+else
+{
+	alert("Extraño enero y febrero");
+}
+*/
+
+
+
+
+
+/*5-Realizar el algoritmo que al presionar el botón "Mostrar" pida un mes del año por prompt,*/
+
+
+switch (mes)
+{
+	
+	case  "enero":
+	case "febrero":
+    
+
+	 	alert("“Veranito!!");
+
+	 	break;
+
+	 default:
+	 
+	    alert("Extraño el verano");	
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
